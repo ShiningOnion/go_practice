ab1c2cfe7d9e6c1a38962fecd3802d478fb4cad0
ShiningOnion <50651414+ShiningOnion@users.noreply.github.com>
2022-03-10 14:28:11 +0800

    typo for switch folder

--- p0003_switch/main.go
+++ p0003_switch/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/ShiningOnion/go_practice/p0003_switch/switchSample"
+)
+
 func main() {
 	switchSample.CheckSwitchUse()
 	switchSample.CheckSwitchWithInterface()
